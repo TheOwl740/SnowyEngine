fabb41d970ac42f8fe2866cae3402c6663bf64c2
Henry Shaw <103895027+TheOwl740@users.noreply.github.com>
2022-05-03 09:27:04 -0400

    cleaned canvas module

--- engine.js
+++ engine.js
@@ -1,44 +1,44 @@
 //jshint maxerr: 10000
 class canvasModule {
-	initialise() {
+	constructor() {
 		document.body.innerHTML = "<canvas id=\"canvas\"></canvas>" + document.body.innerHTML;
 		document.head.innerHTML += "<meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width\"><style>canvas{margin:0;border:0;padding:0;}body{margin:0;overflow:hidden;}</style>";
-		canvas.element = document.getElementById("canvas");
-		canvas.cx = canvas.element.getContext("2d");
-		canvas.element.width = window.innerWidth;
-		canvas.w = window.innerWidth;
-		canvas.element.height = JSON.parse(JSON.stringify(window.innerHeight));
-		canvas.h = window.innerHeight;
-  }
+		this.element = document.getElementById("canvas");
+		this.cx = this.element.getContext("2d");
+		this.element.width = JSON.parse(JSON.stringify(window.innerWidth));
+		this.w = window.innerWidth;
+		this.element.height = JSON.parse(JSON.stringify(window.innerHeight));
+		this.h = window.innerHeight;
+	}
   setDimensions(w, h) {
   	if(w === "full") {
-   	  canvas.element.width = window.innerWidth;
-      canvas.w = window.innerWidth;
-      canvas.element.height = window.innerHeight;
-      canvas.h = window.innerHeight;
+   	  this.element.width = window.innerWidth;
+      this.w = window.innerWidth;
+      this.element.height = window.innerHeight;
+      this.h = window.innerHeight;
     } else {
-      canvas.w = w;
-      canvas.h = h;
-      canvas.element.width = w;
-      canvas.element.height = h;
+      this.w = w;
+      this.h = h;
+      this.element.width = w;
+      this.element.height = h;
     }
   }
   rect(color, alpha, x, y, w, h, r, xOffset, yOffset, lw, filled) {
-    canvas.cx.beginPath();
-    canvas.cx.globalAlpha = alpha;
-    canvas.cx.save();
-    canvas.cx.translate(x, y);
-    canvas.cx.rotate(r * (Math.PI/180));
-    canvas.cx.lineWidth = lw;
+    this.cx.beginPath();
+    this.cx.globalAlpha = alpha;
+    this.cx.save();
+    this.cx.translate(x, y);
+    this.cx.rotate(r * (Math.PI/180));
+    this.cx.lineWidth = lw;
     if(filled) {
-      canvas.cx.fillStyle = color;
-      canvas.cx.fillRect(xOffset - (w / 2), yOffset - (h / 2), w, h);
+      this.cx.fillStyle = color;
+      this.cx.fillRect(xOffset - (w / 2), yOffset - (h / 2), w, h);
     } else {
-      canvas.cx.strokeStyle = color;
-      canvas.cx.rect(xOffset - (w / 2), yOffset - (h / 2), w, h)
-      canvas.cx.stroke();
+      this.cx.strokeStyle = color;
+      this.cx.rect(xOffset - (w / 2), yOffset - (h / 2), w, h)
+      this.cx.stroke();
     }
-    canvas.cx.restore();
+    this.cx.restore();
   }
   createImage(source) {
     var rv = new Image();
@@ -46,59 +46,59 @@ class canvasModule {
     return rv;
   }
   image(source, alpha, x, y, w, h, r, xOffset, yOffset, hFlip, vFlip) {
-    canvas.cx.globalAlpha = alpha;
+    this.cx.globalAlpha = alpha;
     var fc = {
       x: 1,
       y: 1
     };
-    canvas.cx.save();
+    this.cx.save();
     if(hFlip) {
-      canvas.cx.scale(-1, 1);
+      this.cx.scale(-1, 1);
       fc.x = -1;
     } else {
-      canvas.cx.scale(1, 1);
+      this.cx.scale(1, 1);
     }
     if(vFlip) {
-      canvas.cx.scale(1, -1);
+      this.cx.scale(1, -1);
       fc.y = -1;
     } else {
-      canvas.cx.scale(1, 1);
+      this.cx.scale(1, 1);
     }
-    canvas.cx.translate(x * fc.x, y * fc.y);
-    canvas.cx.rotate(r * fc.x * fc.y * (Math.PI/180));
-    canvas.cx.drawImage(source, (xOffset * fc.x) - (w / 2), (yOffset * fc.y) - (h / 2), w, h);
-    canvas.cx.restore();
+    this.cx.translate(x * fc.x, y * fc.y);
+    this.cx.rotate(r * fc.x * fc.y * (Math.PI/180));
+    this.cx.drawImage(source, (xOffset * fc.x) - (w / 2), (yOffset * fc.y) - (h / 2), w, h);
+    this.cx.restore();
   }
   arc(color, alpha, x, y, radius, start, end, lw, filled) {
-  	canvas.cx.globalAlpha = alpha;
-  	canvas.cx.beginPath();
-  	canvas.cx.lineWidth = lw;
-  	canvas.cx.arc(x, y, radius, (start / 50) * Math.PI, (end / 50) * Math.PI, false);
+  	this.cx.globalAlpha = alpha;
+  	this.cx.beginPath();
+  	this.cx.lineWidth = lw;
+  	this.cx.arc(x, y, radius, (start / 50) * Math.PI, (end / 50) * Math.PI, false);
   	if(filled) {
-  		canvas.cx.fillStyle = color;
-  		canvas.cx.fill();
+  		this.cx.fillStyle = color;
+  		this.cx.fill();
   	}
-  	canvas.cx.strokeStyle = color;
-  	canvas.cx.stroke();
+  	this.cx.strokeStyle = color;
+  	this.cx.stroke();
 	}
   text(color, font, text, alpha, x, y, size, r, xOffset, yOffset) {
-    canvas.cx.save();
-    canvas.cx.translate(x, y);
-    canvas.cx.rotate(r * (Math.PI/180));
-    canvas.cx.globalAlpha = alpha;
-    canvas.cx.font = size + "px " + font;
-    canvas.cx.fillStyle = color;
-    canvas.cx.fillText(text, xOffset, yOffset);
-    canvas.cx.restore();
+    this.cx.save();
+    this.cx.translate(x, y);
+    this.cx.rotate(r * (Math.PI/180));
+    this.cx.globalAlpha = alpha;
+    this.cx.font = size + "px " + font;
+    this.cx.fillStyle = color;
+    this.cx.fillText(text, xOffset, yOffset);
+    this.cx.restore();
   }
   line(color, alpha, x1, y1, x2, y2, w) {
-    canvas.cx.globalAlpha = alpha;
-    canvas.cx.strokeStyle = color;
-    canvas.cx.lineWidth = w;
-    canvas.cx.beginPath();
-    canvas.cx.moveTo(x1, y1);
-    canvas.cx.lineTo(x2, y2);
-    canvas.cx.stroke();
+    this.cx.globalAlpha = alpha;
+    this.cx.strokeStyle = color;
+    this.cx.lineWidth = w;
+    this.cx.beginPath();
+    this.cx.moveTo(x1, y1);
+    this.cx.lineTo(x2, y2);
+    this.cx.stroke();
   }
   createSprite (source, columns, rows, w, h) {
     return {
@@ -110,24 +110,24 @@ class canvasModule {
     };
   }
   drawSprite(sprite, alpha, column, row, x, y, w, h, r, xOffset, yOffset) {
-    canvas.cx.globalAlpha = alpha;
+    this.cx.globalAlpha = alpha;
     var image = sprite.source
-    canvas.cx.save();
+    this.cx.save();
     if(hFlip) {
-      canvas.cx.scale(-1, 1);
+      this.cx.scale(-1, 1);
       fc.x = -1;
     } else {
-      canvas.cx.scale(1, 1);
+      this.cx.scale(1, 1);
     }
     if(vFlip) {
-      canvas.cx.scale(1, -1);
+      this.cx.scale(1, -1);
       fc.y = -1;
     } else {
-      canvas.cx.scale(1, 1);
+      this.cx.scale(1, 1);
     }
-    canvas.cx.translate(x * fc.x, y * fc.y);
-    canvas.cx.rotate(r * fc.x * fc.y * (Math.PI/180));
-    canvas.cx.drawImage(image, column * (sprite.w / sprite.columns), row * (sprite.h / sprite.rows), sprite.w / sprite.columns, sprite.h / sprite.rows, (xOffset * fc.x) - (w / 2), (yOffset * fc.y) - (h / 2), w, h);
-    canvas.cx.restore();
+    this.cx.translate(x * fc.x, y * fc.y);
+    this.cx.rotate(r * fc.x * fc.y * (Math.PI/180));
+    this.cx.drawImage(image, column * (sprite.w / sprite.columns), row * (sprite.h / sprite.rows), sprite.w / sprite.columns, sprite.h / sprite.rows, (xOffset * fc.x) - (w / 2), (yOffset * fc.y) - (h / 2), w, h);
+    this.cx.restore();
   }
 }
