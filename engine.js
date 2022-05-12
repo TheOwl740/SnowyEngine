29a5741231c42d5b1c92f7eebe2b18cb3a60d1da
Henry Shaw <103895027+TheOwl740@users.noreply.github.com>
2022-05-12 08:57:32 -0400

    Cleaned

--- engine.js
+++ engine.js
@@ -1,140 +1,24 @@
 //jshint maxerr: 10000
-document.addEventListener("keydown", press);
-document.addEventListener("keyup", up);
-document.addEventListener("mousemove", move);
-document.addEventListener("mousedown", click);
-document.addEventListener("mouseup", unclick);
+
+//CONSTRUCT DOCUMENT
+document.body.innerHTML = "<canvas id=\"canvas\"></canvas>" + document.body.innerHTML;
+document.head.innerHTML += "<meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width\"><style>canvas{margin:0;border:0;padding:0;}body{margin:0;overflow:hidden;}</style>";
 
 var e = {
 	c: {
-		document.body.innerHTML = "<canvas id=\"canvas\"></canvas>" + document.body.innerHTML;
-		document.head.innerHTML += "<meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width\"><style>canvas{margin:0;border:0;padding:0;}body{margin:0;overflow:hidden;}</style>";
-		element = document.getElementById("canvas");
-		cx = this.element.getContext("2d");
-		element.width = window.innerWidth;
-		w = window.innerWidth;
-		element.height = window.innerHeight;
-		h = window.innerHeight;
-		setDimensions: (w, h) => {
-			if(w === "full") {
-				this.element.width = window.innerWidth;
-				this.w = window.innerWidth;
-				this.element.height = window.innerHeight;
-				this.h = window.innerHeight;
-			} else {
-				this.w = w;
-				this.h = h;
-				this.element.width = w;
-				this.element.height = h;
-			}
-		},
-		rect: (color, alpha, x, y, w, h, r, xOffset, yOffset, lw, filled) => {
-			this.cx.beginPath();
-			this.cx.globalAlpha = alpha;
-			this.cx.save();
-			this.cx.translate(x, y);
-			this.cx.rotate(r * (Math.PI/180));
-			this.cx.lineWidth = lw;
-			if(filled) {
-				this.cx.fillStyle = color;
-				this.cx.fillRect(xOffset - (w / 2), yOffset - (h / 2), w, h);
-			} else {
-				this.cx.strokeStyle = color;
-				this.cx.rect(xOffset - (w / 2), yOffset - (h / 2), w, h)
-				this.cx.stroke();
-			}
-			this.cx.restore();
-		},
-		createImage: (source) => {
-			var rv = new Image();
-			rv.src = source;
-			return rv;
-		}
-		image: (source, alpha, x, y, w, h, r, xOffset, yOffset, hFlip, vFlip) => {
-			this.cx.globalAlpha = alpha;
-			var fc = {
-				x: 1,
-				y: 1
-			};
-			this.cx.save();
-			if(hFlip) {
-				this.cx.scale(-1, 1);
-				fc.x = -1;
-			} else {
-				this.cx.scale(1, 1);
-			}
-			if(vFlip) {
-				this.cx.scale(1, -1);
-				fc.y = -1;
-			} else {
-				this.cx.scale(1, 1);
-			}
-			this.cx.translate(x * fc.x, y * fc.y);
-			this.cx.rotate(r * fc.x * fc.y * (Math.PI/180));
-			this.cx.drawImage(source, (xOffset * fc.x) - (w / 2), (yOffset * fc.y) - (h / 2), w, h);
-			this.cx.restore();
-		}
-		arc: (color, alpha, x, y, radius, start, end, lw, filled) => {
-			this.cx.globalAlpha = alpha;
-			this.cx.beginPath();
-			this.cx.lineWidth = lw;
-			this.cx.arc(x, y, radius, (start / 180) * Math.PI, (end / 180) * Math.PI, false);
-			if(filled) {
-				this.cx.fillStyle = color;
-				this.cx.fill();
-			}
-			this.cx.strokeStyle = color;
-			this.cx.stroke();
-		}
-		text: (color, font, text, alpha, x, y, size, r, xOffset, yOffset) => {
-			this.cx.save();
-			this.cx.translate(x, y);
-			this.cx.rotate(r * (Math.PI/180));
-			this.cx.globalAlpha = alpha;
-			this.cx.font = size + "px " + font;
-			this.cx.fillStyle = color;
-			this.cx.fillText(text, xOffset, yOffset);
-			this.cx.restore();
-		}
-		line: (color, alpha, x1, y1, x2, y2, w) => {
-			this.cx.globalAlpha = alpha;
-			this.cx.strokeStyle = color;
-			this.cx.lineWidth = w;
-			this.cx.beginPath();
-			this.cx.moveTo(x1, y1);
-			this.cx.lineTo(x2, y2);
-			this.cx.stroke();
-		}
-		createSprite: (source, columns, rows, w, h) => {
-			return {
-				source: source,
-				columns: columns,
-				rows: rows,
-				w: w,
-				h: h
-			};
-		}
-		drawSprite: (sprite, alpha, column, row, x, y, w, h, r, xOffset, yOffset, hFlip, vFlip) => {
-			this.cx.globalAlpha = alpha;
-			var image = sprite.source
-			this.cx.save();
-			if(hFlip) {
-				this.cx.scale(-1, 1);
-				fc.x = -1;
-			} else {
-				this.cx.scale(1, 1);
-			}
-			if(vFlip) {
-				this.cx.scale(1, -1);
-				fc.y = -1;
-			} else {
-				this.cx.scale(1, 1);
-			}
-			this.cx.translate(x * fc.x, y * fc.y);
-			this.cx.rotate(r * fc.x * fc.y * (Math.PI/180));
-			this.cx.drawImage(image, column * (sprite.w / sprite.columns), row * (sprite.h / sprite.rows), sprite.w / sprite.columns, sprite.h / sprite.rows, (xOffset * fc.x) - (w / 2), (yOffset * fc.y) - (h / 2), w, h);
-			this.cx.restore();
-		}
+		element: document.getElementById("canvas"),
+		cx: document.getElementById("canvas").getContext("2d"),
+		w: window.innerWidth,
+		h: window.innerHeight,
+		setDimensions: null,
+		rect: null,
+		createImage: null,
+		image: null,
+		arc: null,
+		text: null,
+		line: null,
+		createSprite: null,
+		drawSprite: null
 	},
 	i: {
 		mouse: {
@@ -142,29 +26,8 @@ var e = {
 			y: 0,
 			clicking: false
 		},
-		this.pressedKeys = [
-		],
-		newKey: (e) => {
-			if(!input.pressedKeys.includes(e.key)) {
-				this.pressedKeys.push(e.key);
-			}
-		},
-		removeKey: (e) => {
-			this.pressedKeys.splice(this.pressedKeys.indexOf(e.key), 1);
-		},
-		getKey: (key) => {
-			return this.pressedKeys.includes(key);
-		},
-		locateMouse: (e) => {
-			this.mouse.x = e.clientX;
-			this.mouse.y = e.clientY;
-		},
-		mDown: () => {
-			this.mouse.clicking = true;
-		},
-		mUp: () => {
-			this.mouse.clicking = false;
-		}
+		pressedKeys: [
+		]
 	},
 	m: {
 		distance: (x1, y1, x2, y2) => {
@@ -206,10 +69,156 @@ var e = {
 	t: {
 		timerObject: null,
 		start: (speed, updateFunction) => {
-			this.timerObject = setInterval(updateFunction, speed);
-		}
+			timerObject = setInterval(updateFunction, speed);
+		},
 		stop: () => {
 			clearInterval(this.timerObject);
 		}
 	}
 }
+
+//CANVAS FUNCTIONS
+e.c.setDimensions = (w, h) => {
+	if(w === "full") {
+		e.c.element.width = window.innerWidth;
+		e.c.w = window.innerWidth;
+		e.c.element.height = window.innerHeight;
+		e.c.h = window.innerHeight;
+	} else {
+		e.c.w = w;
+		e.c.h = h;
+		e.c.element.width = w;
+		e.c.element.height = h;
+	}
+},
+e.c.rect = (color, alpha, x, y, w, h, r, xOffset, yOffset, lw, filled) => {
+	e.c.cx.beginPath();
+	e.c.cx.globalAlpha = alpha;
+	e.c.cx.save();
+	e.c.cx.translate(x, y);
+	e.c.cx.rotate(r * (Math.PI/180));
+	e.c.cx.lineWidth = lw;
+	if(filled) {
+		e.c.cx.fillStyle = color;
+		e.c.cx.fillRect(xOffset - (w / 2), yOffset - (h / 2), w, h);
+	} else {
+		e.c.cx.strokeStyle = color;
+		e.c.cx.rect(xOffset - (w / 2), yOffset - (h / 2), w, h)
+		e.c.cx.stroke();
+	}
+	e.c.cx.restore();
+},
+e.c.createImage = (source) => {
+	var rv = new Image();
+	rv.src = source;
+	return rv;
+},
+e.c.image = (source, alpha, x, y, w, h, r, xOffset, yOffset, hFlip, vFlip) => {
+	e.c.cx.globalAlpha = alpha;
+	var fc = {
+		x: 1,
+		y: 1
+	};
+	e.c.cx.save();
+	if(hFlip) {
+		e.c.cx.scale(-1, 1);
+		fc.x = -1;
+	} else {
+		e.c.cx.scale(1, 1);
+	}
+	if(vFlip) {
+		e.c.cx.scale(1, -1);
+	fc.y = -1;
+	} else {
+		e.c.cx.scale(1, 1);
+	}
+	e.c.cx.translate(x * fc.x, y * fc.y);
+	e.c.cx.rotate(r * fc.x * fc.y * (Math.PI/180));
+	e.c.cx.drawImage(source, (xOffset * fc.x) - (w / 2), (yOffset * fc.y) - (h / 2), w, h);
+	e.c.cx.restore();
+},
+e.c.arc = (color, alpha, x, y, radius, start, end, lw, filled) => {
+	e.c.cx.globalAlpha = alpha;
+	e.c.cx.beginPath();
+	e.c.cx.lineWidth = lw;
+	e.c.cx.arc(x, y, radius, (start / 180) * Math.PI, (end / 180) * Math.PI, false);
+	if(filled) {
+		e.c.cx.fillStyle = color;
+		e.c.cx.fill();
+	}	
+	e.c.cx.strokeStyle = color;
+	e.c.cx.stroke();
+},
+e.c.text = (color, font, text, alpha, x, y, size, r, xOffset, yOffset) => {
+	e.c.cx.save();
+	e.c.cx.translate(x, y);
+	e.c.cx.rotate(r * (Math.PI/180));
+	e.c.cx.globalAlpha = alpha;
+	e.c.cx.font = size + "px " + font;
+	e.c.cx.fillStyle = color;
+	e.c.cx.fillText(text, xOffset, yOffset);
+	e.c.cx.restore();	
+},
+e.c.line = (color, alpha, x1, y1, x2, y2, w) => {
+	e.c.cx.globalAlpha = alpha;
+	e.c.cx.strokeStyle = color;
+	e.c.cx.lineWidth = w;
+	e.c.cx.beginPath();
+	e.c.cx.moveTo(x1, y1);
+	e.c.cx.lineTo(x2, y2);
+	e.c.cx.stroke();
+},
+e.c.createSprite = (source, columns, rows, w, h) => {
+	return {
+		source: source,
+		columns: columns,
+		rows: rows,
+		w: w,
+		h: h
+	};
+},
+e.c.drawSprite = (sprite, alpha, column, row, x, y, w, h, r, xOffset, yOffset, hFlip, vFlip) => {
+	e.c.cx.globalAlpha = alpha;
+	var image = sprite.source
+	e.c.cx.save();
+	if(hFlip) {
+		e.c.cx.scale(-1, 1);
+		fc.x = -1;
+	} else {
+		e.c.cx.scale(1, 1);
+	}
+	if(vFlip) {
+		e.c.cx.scale(1, -1);
+		fc.y = -1;
+	} else {
+		e.c.cx.scale(1, 1);
+	}
+	e.c.cx.translate(x * fc.x, y * fc.y);
+	e.c.cx.rotate(r * fc.x * fc.y * (Math.PI/180));
+	e.c.cx.drawImage(image, column * (sprite.w / sprite.columns), row * (sprite.h / sprite.rows), sprite.w / sprite.columns, sprite.h / sprite.rows, (xOffset * fc.x) - (w / 2), (yOffset * fc.y) - (h / 2), w, h);
+	e.c.cx.restore();
+}
+
+//ADD EVENT LISTENERS
+document.addEventListener("keydown", (eObj) => {
+	if(!e.i.pressedKeys.includes(eObj.key)) {
+		e.i.pressedKeys.push(eObj.key);
+	}
+});
+document.addEventListener("keyup", (eObj) => {
+	e.i.pressedKeys.splice(e.i.pressedKeys.indexOf(eObj.key), 1);
+});
+document.addEventListener("mousemove", (eObj) => {
+	e.i.mouse.x = eObj.clientX;
+	e.i.mouse.y = eObj.clientY;
+});
+document.addEventListener("mousedown", mDown = () => {
+	e.i.mouse.clicking = true;
+});
+document.addEventListener("mouseup", () => {
+	e.i.mouse.clicking = false;
+});
+
+//SET FULL CANVAS DIMENSIONS
+e.c.element.width = window.innerWidth;
+e.c.element.height = window.innerHeight;
